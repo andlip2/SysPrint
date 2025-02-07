b6c24d8bb2850d07f6a8c90ada2805f6dc2f2bde
Ana Beatriz <anabiavs@hotmail.com>
2025-02-07 08:56:55 -0300

    Removendo interação com a coluna "TotalPages"
    Removendo interação com a coluna "TotalPages", pois a mesma não continuará sendo utilizada.

--- atualiza_user.py
+++ atualiza_user.py
@@ -9,42 +9,31 @@ def update_user_totals(user, pages, DEFAULT_PRINT_LIMIT, engine, departamento):
             transaction = connection.begin()
 
             # Verificar se o usuário já existe na tabela
-            select_query = (
-                "SELECT TotalPages, PrintLimit, Blocked FROM users WHERE User = :user"
-            )
+            select_query = "SELECT PrintLimit, Blocked FROM users WHERE User = :user"
             result = connection.execute(text(select_query), {"user": user}).fetchone()
 
             if result:
-                # Se o usuário já existir, atualizar o total de páginas
-                new_total = pages
-
-                # Caso o limite não tenha sido alcançado, só atualiza o total de páginas
-                update_query = (
-                    "UPDATE users SET TotalPages = :new_total WHERE User = :user"
-                )
-                connection.execute(
-                    text(update_query), {"new_total": new_total, "user": user}
-                )
-                print(f"Total de páginas atualizado para {user}: {new_total}")
+                print(f"Usuário {user} já registrado no sistema.")
 
             else:
                 # Se o usuário não existir, inserir um novo registro com o limite de impressão e service_on = 0
                 insert_query = """
-                INSERT INTO users (User, TotalPages, PrintLimit, Blocked, Department) 
-                VALUES (:user, :pages, :print_limit, FALSE, :department)
+                INSERT INTO users (User, PrintLimit, Blocked, Department) 
+                VALUES (:user, :print_limit, FALSE, :department)
                 """
                 connection.execute(
                     text(insert_query),
-                    {"user": user, "pages": pages, "print_limit": DEFAULT_PRINT_LIMIT, "department": departamento},
+                    {
+                        "user": user,
+                        "print_limit": DEFAULT_PRINT_LIMIT,
+                        "department": departamento,
+                    },
                 )
                 print(
-                    f"Novo registro inserido para {user}: {pages} páginas, Limite de impressão: {DEFAULT_PRINT_LIMIT} páginas departamento: {departamento}."
+                    f"Novo registro inserido para {user}: Limite de impressão: {DEFAULT_PRINT_LIMIT}, Departamento: {departamento}."
                 )
 
             # Commit da transação
             transaction.commit()
     except Exception as e:
-        print(f"Erro ao atualizar os totais de páginas para o usuário {user}: {e}")
-
-
-
+        print(f"Erro ao atualizar os dados para o usuário {user}: {e}")
