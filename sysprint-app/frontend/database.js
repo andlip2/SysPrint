755656a357bc2502904f2304cd28ea5bad65150d
Ana Beatriz <anabiavs@hotmail.com>
2025-03-10 11:58:00 -0300

    Modificação na tela "Banco de Dados"
    - Adicionando tabela de usuários

--- sysprint-app/frontend/database.js
+++ sysprint-app/frontend/database.js
@@ -1,7 +1,8 @@
 async function fetchUsersData() {
     try {
-        const response = await fetch("http://127.0.0.1:5000/get-users");
-        const data = await response.json();  // Dados que vêm com as chaves 'id', 'name' e 'email'
+        const response = await fetch("http://127.0.0.1:5000/users-data");
+        const data = await response.json();
+        console.log("Dados recebidos:", data);  // Verificar se os dados estão corretos
         renderUsersTable(data);
     } catch (error) {
         console.error("Erro ao carregar os dados da tabela de usuários:", error);
@@ -9,14 +10,18 @@ async function fetchUsersData() {
 }
 
 function renderUsersTable(data) {
-    const table = document.getElementById('users');
-    table.innerHTML = '';  // Limpa a tabela antes de adicionar os novos dados
+    const tableBody = document.querySelector("#users-table tbody");
+    tableBody.innerHTML = ''; // Limpa o tbody antes de adicionar os dados
 
-    data.forEach(users => {
-        const row = document.createElement('tr');
-        row.innerHTML = `
-            <td>${users.User}</td>   <!-- Exibindo a coluna id -->
-        `;
-        table.appendChild(row);  // Adiciona a linha na tabela
+    data.forEach(user => {
+        const row = tableBody.insertRow();
+        row.insertCell(0).textContent = user.user;
+        row.insertCell(1).textContent = user.total_pages;
+        row.insertCell(2).textContent = user.print_limit;
+        row.insertCell(3).textContent = user.blocked ? 'Sim' : 'Não';
+        row.insertCell(4).textContent = user.department;
     });
 }
+
+// Garantir que a função é chamada quando a página carregar
+window.onload = fetchUsersData;
