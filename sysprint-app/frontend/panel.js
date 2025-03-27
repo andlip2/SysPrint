3c1ade3ccfef6ca4340a079e5619e587467a70a8
Ana Beatriz <anabiavs@hotmail.com>
2025-03-27 11:57:36 -0300

    Adicionando painel de controle na interface

---
+++ sysprint-app/frontend/panel.js
@@ -0,0 +1,14 @@
+function goToUsers() {
+    window.location.href = "users.html"; // Redireciona para a página de login
+}
+
+function goToDepartments() {
+    window.location.href = "departments.html";
+}
+function goToPrinters() {
+    window.location.href = "printers.html";
+}
+
+function logout() {
+    window.location.href = "login.html"; // Redireciona para a página de login
+}
